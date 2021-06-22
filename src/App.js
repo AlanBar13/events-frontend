d43a8fd9ff178ce8dfb0aa79ba2824b7ae0dfd29
Alan Bardales <alan.g.bardales@gmail.com>
2021-06-21 21:50:08 -0500

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
+import HomeScreen from "./screens/HomeScreen";
+import CalendarScreen from "./screens/CalendarScreen";
+import LoginScreen from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={HomeScreen}></Route>
+      <Route path="/calendar" component={CalendarScreen}></Route>
+      <Route path="/login" component={LoginScreen}></Route>
+      <Route path="/profile" component={ProfileScreen}></Route>
+    </Router>
   );
 }
 
---
+++ src/screens/CalendarScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CalendarScreen() {
+    return (
+        <div>
+            CalendarScreen
+        </div>
+    )
+}
+
+export default CalendarScreen
